611a163a60eeb0f0a2138ec057f852afcbd4e9ac
aqideh <123452394+aqideh@users.noreply.github.com>
2026-05-07 12:46:20 +0800

    Add GitHub Pages Supabase config

---
+++ assets/supabase-config.js
@@ -0,0 +1,5 @@
+window.MENDAKI_SUPABASE_CONFIG = {
+  url: 'https://xyrcdukmubctqddgkfsi.supabase.co',
+  anonKey: 'sb_publishable_aZYoXIn-nHjOWp62-vEXwg_lzV2XCYx',
+  authRedirectTo: 'https://aqideh.github.io/mendakivolapp/'
+};
